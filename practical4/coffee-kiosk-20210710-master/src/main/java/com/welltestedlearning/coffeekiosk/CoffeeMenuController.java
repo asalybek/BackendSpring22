ff166e10aef54bfb4073d4ebd59afd990f520958
arsalybek <araylymsalybek@gmail.com>
2022-03-06 00:49:04 +0600

    sis 1

--- practical4/coffee-kiosk-20210710-master/src/main/java/com/welltestedlearning/coffeekiosk/CoffeeMenuController.java
+++ practical4/coffee-kiosk-20210710-master/src/main/java/com/welltestedlearning/coffeekiosk/CoffeeMenuController.java
@@ -1,2 +1,13 @@
-package com.welltestedlearning.coffeekiosk;public class CoffeeMenuController {
+package com.welltestedlearning.coffeekiosk;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoffeeMenuController {
+
+    @GetMapping("/api/coffee/menu/sizes")
+    public String getCoffeeSizes() {
+        return "small";
+    }
 }
